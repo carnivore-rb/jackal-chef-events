caddefbf69c2b77c0768b52ff9b776db4954fe74
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-22 11:39:55 -0800

    Add callback for direct instance resource deletion

--- lib/jackal-chef-events.rb
+++ lib/jackal-chef-events.rb
@@ -4,5 +4,6 @@ require 'jackal-chef-events/version'
 module Jackal
   module ChefEvents
     autoload :StackDelete, 'jackal-chef-events/stack_delete'
+    autoload :StackInstanceDelete, 'jackal-chef-events/stack_instance_delete'
   end
 end
